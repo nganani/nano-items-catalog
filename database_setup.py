be3c48a6989775cd17bd780db95390521ecff8ac
Nir Ganani <nganani@gmail.com>
2018-07-28 15:45:33 +0300

    initial commit

---
+++ database_setup.py
@@ -0,0 +1,76 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False)
+    picture = Column(String(255))
+
+    @property
+    def serialize(self):
+        # Return object data in easily 'serializeable' format
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture
+        }
+
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        # Return object data in easily 'serializeable' format
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    description = Column(String(255))
+
+    category_id = Column(Integer,
+        ForeignKey('category.id'), nullable=False)
+    category = relationship(
+        "Category", backref=backref("Item", cascade="all, delete"))
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        # Return object data in easily 'serializeable' format
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'category_id': self.category_id,
+            'user_id': self.user_id
+        }
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
